2b69c133f65992f1f493ec0c7927cdd189792ae9
Lidia Alonso <l.alonsodiaz@gmail.com>
2020-11-23 17:56:14 +0100

    added first 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapaComponent } from './mapa/mapa.component';
+import { FiltroComponent } from './filtro/filtro.component';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapaComponent,
+    FiltroComponent,
+    CalendarioComponent,
+    TiendaComponent
   ],
   imports: [
     BrowserModule,
