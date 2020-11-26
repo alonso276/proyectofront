0f10735745052def2f32a0834d3a244749e54763
msotomail <msotomail@gmail.com>
2020-11-26 16:40:12 +0100

    ms- conflictos aceptados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MapaComponent } from './mapa/mapa.component';
+
 import { FiltroComponent } from './filtro/filtro.component';
 import { CalendarioComponent } from './calendario/calendario.component';
 import { TiendaComponent } from './tienda/tienda.component';
@@ -15,16 +15,12 @@ import { FormProductoComponent } from './form-producto/form-producto.component';
 import { LoginComponent } from './login/login.component';
 import { ProductorDetalleComponent } from './productor-detalle/productor-detalle.component';
 import { Error404Component } from './error404/error404.component';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
-=======
->>>>>>> feature_l
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MapaComponent,
     FiltroComponent,
     CalendarioComponent,
     TiendaComponent,
